0124cb74545d08fb5d57407e5622128e00de5944
John Doe <gmiello411@gmail.com>
2025-03-12 19:18:24 -0300

    my first php commit

---
+++ segundaAula/meunome.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Meu nome é: duda";
+
+?>
---
+++ segundaAula/sintaxe.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "teste php";
+
+
+?>
+
+o que eu escrever fora da tag vai rodar como um texto apenas e não será interpretado como código php
